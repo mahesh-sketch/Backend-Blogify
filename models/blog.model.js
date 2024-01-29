6d10014d314bf5b7e355280f855ac0382516e9f4
mahesh dash <maheshdash84821@gmail.com>
2024-01-30 00:00:11 +0530

    Blog Model

---
+++ models/blog.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const blogSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    coverImageURL: {
+      type: String,
+      required: false,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = model("Blog", blogSchema);
+
+export default Blog;
