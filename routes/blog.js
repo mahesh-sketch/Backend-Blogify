280f158506a59d2d279ecf89f8389b732879673f
mahesh dash <maheshdash84821@gmail.com>
2024-01-29 23:58:33 +0530

    Blog Route

---
+++ routes/blog.js
@@ -0,0 +1,58 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+import Blog from "../models/blog.model.js";
+import Comment from "../models/comment.model.js";
+
+const blogRouter = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+blogRouter.get("/add-new", (req, res) => {
+  res.render("addBlog", {
+    user: req.user,
+  });
+});
+
+blogRouter.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+blogRouter.post("/comment/:blogId", async (req, res) => {
+  const comment = await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
+
+blogRouter.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    body,
+    title,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
+});
+export default blogRouter;
